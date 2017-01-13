f83784939b8516078994eb572487950897b5fd44
Siva Sundaramoorthy <siva11.sundaramoorthy@gmail.com>
2017-01-13 14:39:26 +0530

    synapses

--- NeuralNetworks/NeuralNet.py
+++ NeuralNetworks/NeuralNet.py
@@ -23,3 +23,12 @@ y = np.array([[0],
               [0]
              ])
 
+
+#Useful for debugging and lots of other purposes
+np.random.seed(1)
+
+#synapses
+syn0 = 2*np.random.random((3,4)) - 1  # 3x4 matrix of weights ((2 inputs + 1 bias) x 4 nodes in the hidden layer)
+syn1 = 2*np.random.random((4,1)) - 1  # 4x1 matrix of weights. (4 nodes x 1 output) - no bias term in the hidden layer.
+
+
