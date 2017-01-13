dff7400cb15b15c8a42ec10715da43638c6fe2b0
Siva Sundaramoorthy <siva11.sundaramoorthy@gmail.com>
2017-01-13 14:47:39 +0530

    Cleaned code. Todo: Add doc

--- NeuralNetworks/NeuralNet.py
+++ NeuralNetworks/NeuralNet.py
@@ -1,21 +1,21 @@
-#The program creates an neural network that simulates the 
-#exclusive OR function with two inputs and one output. 
+"""
+The program creates an neural network that simulates the 
+exclusive OR function with two inputs and one output. 
+"""
 
 import numpy as np
 
-#non-linear sigmoid function
+#Non-linear sigmoid function
 def nonlinear(x, deriv=False):
     if (deriv == True):
-        return x*(1-x)
-    
+        return x*(1-x)    
     return 1/(1+np.exp(-x))
 
-#input data
+#Input data
 X=np.array([ [0,0,1],
              [0,1,1],
              [1,0,1],
              [1,1,1]])
-
 #Output data
 y = np.array([[0],
               [1],
@@ -23,20 +23,16 @@ y = np.array([[0],
               [0]
              ])
 
-
 #Useful for debugging and lots of other purposes
 np.random.seed(1)
 
-#synapses
+#Synapses
 syn0 = 2*np.random.random((3,4)) - 1  # 3x4 matrix of weights ((2 inputs + 1 bias) x 4 nodes in the hidden layer)
 syn1 = 2*np.random.random((4,1)) - 1  # 4x1 matrix of weights. (4 nodes x 1 output) - no bias term in the hidden layer.
 
-
-#training proces starts here
+#Training proces starts here
 #60000 iterations / epochs
-for j in xrange(60000):
-        
-    
+for j in xrange(60000):           
     # Calculate forward through the network.
     l0 = X
     l1 = nonlinear(np.dot(l0, syn0))
@@ -53,13 +49,10 @@ for j in xrange(60000):
     # Only print the error every 10000 steps, to save time and limit the amount of output. 
     if(j % 10000==0):
         print "Error:" + str(np.mean(np.abs(l2_error)))
-    
-    
+        
     #Back Propogation starts 
-    l2_delta = l2_error*nonlinear(l2, deriv=True )
-    
-    l1_error = np.dot(l2_delta, syn1.T)
-    
+    l2_delta = l2_error*nonlinear(l2, deriv=True )    
+    l1_error = np.dot(l2_delta, syn1.T)    
     l1_delta = l1_error * nonlinear(l1,deriv=True )
         
     #update weights (no learning rate term)
