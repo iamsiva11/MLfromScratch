b73f8d1143697021fae85435e6eaf567f66690c7
Siva Sundaramoorthy <siva11.sundaramoorthy@gmail.com>
2017-01-17 13:03:45 +0530

    test pca

--- PCA/pca.py
+++ PCA/pca.py
@@ -17,3 +17,16 @@ class PCA:
 
 	def transform(self,X):
 		return self.w.dot(X.T).T
+
+if __name__=="__main__":
+
+	#Input matrix X
+	X = np.array([ [ 8.5, 79 , 3.5, 0],
+               [ 8.5, 86 ,5.5, 1],
+               [ 3.5, 99 ,6.5, 3],
+               [ 3.5 ,111, 6.5, 1],
+               [ 5.5 ,78 , 8.5 ,2] ])
+
+	pca=PCA(2)
+	pca.fit(X)
+	print pca.transform(X)
